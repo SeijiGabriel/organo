78d97fe7f33b322ebf496f4918310a9435a20968
Gabriel Kanda <gabrielkanda@MacBook-Air-de-Gabriel.local>
2023-09-28 20:33:57 -0300

    first commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import { Fragment } from 'react'
+import './Banner.css'
+
+export const Banner = () => {
+    return (
+        <>
+            <header className="banner">
+                <img src="/imagens/banner.png" alt="Banner organo" />
+            </header>
+        </>
+    )
+}
